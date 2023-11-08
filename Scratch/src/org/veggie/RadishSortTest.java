fbb5db94a0e493a7624b4fce1cdf56dbd71f04fb
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-11-08 10:35:29 -0800

    RadishSortTest with anonymous Comparator<Radish>

--- Scratch/src/org/veggie/RadishSortTest.java
+++ Scratch/src/org/veggie/RadishSortTest.java
@@ -1,6 +1,7 @@
 package org.veggie;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -23,13 +24,36 @@ class RadishSortTest {
         dump(radishes);
         System.out.println();
 
-        System.out.println("sort by color, via RadishColorComparator");
-        radishes.sort(new RadishColorComparator());
+        System.out.println("sort by color, via anonymous Comparator<Radish> class");
+        // radishes.sort(new RadishColorComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return r1.getColor().compareTo(r2.getColor());
+            }
+        });
         dump(radishes);
         System.out.println();
 
-        System.out.println("sort by guysOnTop, via RadishGuysOnTopComparator");
-        radishes.sort(new RadishGuysOnTopComparator());
+        System.out.println("sort by guysOnTop, via anonymous class");
+        // radishes.sort(new RadishGuysOnTopComparator());
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop());
+            }
+        });
+        dump(radishes);
+        System.out.println();
+
+        System.out.println("sort by tailLength, via anonymous class");
+        radishes.sort(new Comparator<Radish>() {
+
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        });
         dump(radishes);
         System.out.println();
     }

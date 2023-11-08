2926b3814b6e35a2fbd36b8242d539430f1e387c
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-11-08 12:18:13 -0800

    EmployeeTest with anonymous subclass instead of name inner class DummyEmployee

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -11,8 +11,18 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp1 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
-        emp2 = new SalariedEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        // emp1 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        // emp2 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+    }
+
+    private Employee getEmployee() {
+        return new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() { return 0; }
+            public double payTaxes() { return 0; }
+        };
     }
 
     @Test
@@ -30,5 +40,16 @@ public class EmployeeTest {
     @Test
     public void equals_shouldReturnTrue_allPropertiesSame() {
         assertEquals(emp1, emp2);
+    }
+
+    // NAMED, MEMBER-LEVEL INNER CLASSES
+    private class DummyEmployee extends Employee {
+
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        public double pay() { return 0; }
+        public double payTaxes() { return 0; }
     }
 }
